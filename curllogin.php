69e1cefe63d49d9297fc5b14e823afddf2b7771a
benny306 <benny306@users.noreply.github.com>
2014-05-13 23:18:43 +0100

    Update curllogin.php

--- curllogin.php
+++ curllogin.php
@@ -1,10 +1,15 @@
 <?php
+//take email and password from user, I didn't account for sql injection because we're sending it to 3rd party
 $name = $_POST['name'];
 $password = $_POST['password'];
+//encode email 
 $namecode = urlencode($name);
 $url="https://users.premierleague.com/PremierUser/redirectLogin"; 
+//set post data for premierleague.com website
 $postdata = "email=$namecode&password=$password&success=http%3A%2F%2Ffantasy.premierleague.com%2Faccounts%2Flogin%2F&fail=http%3A%2F%2Ffantasy.premierleague.com%2F%3Ffail";
+//create cookies file
 $cookie="cookie.txt"; 
+//set CURL variables
 $ch = curl_init(); 
 curl_setopt ($ch, CURLOPT_URL, $url); 
 curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -19,6 +24,7 @@ curl_setopt ($ch, CURLOPT_COOKIEFILE, $cookie);
 curl_setopt ($ch, CURLOPT_REFERER, $url); 
 curl_setopt ($ch, CURLOPT_POSTFIELDS, $postdata); 
 curl_setopt ($ch, CURLOPT_POST, 1); 
+//return result
 $result = curl_exec($ch);
 unset($ch);
 
@@ -45,14 +51,17 @@ if($result)
   </tr>
 <tr>
 <?php        //Logged in
+//create a new dom document
 $doc = new DOMDocument();
 @$doc->loadHTML($result);
 $selector = new DOMXPath($doc);
+//get contents within the ismPitchElement and return playerid
 foreach($selector->query('//@class[starts-with(., "ismPitchElement")]') as $classattr) {
     // remove the prefix using `ltrim()`
     $json = json_decode(ltrim($classattr->nodeValue, "ismPitchElement"));
     $thisid = ($json->id);
 $endhash ='/';
+//scroll through each player id on users team and get info
 $url = 'http://fantasy.premierleague.com/web/api/elements/';
 $urlid = $url . $thisid . $endhash; 
 $results = file_get_contents($urlid); 
