e39c7252a6bde17b9957b77c3015db5f1b184e8d
bennataraj <bennataraj@users.noreply.github.com>
2014-06-22 15:52:40 +0100

    Update getplayerinfo.php

--- getplayerinfo.php
+++ getplayerinfo.php
@@ -3,7 +3,7 @@ $con = mysqli_connect("localhost:3306","root","password","databasename");
 // Check connection
 if (mysqli_connect_errno())
   {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 // Set execution time
 ini_set('max_execution_time', 900); 
@@ -12,26 +12,26 @@ $id = 1;
 for ($id=1; $id<=700; $id++)
   {
 
-$endhash ='/';
-$url = 'http://fantasy.premierleague.com/web/api/elements/';
-$urlid = $url . $id . $endhash; 
-$results = file_get_contents($urlid); 
-$playerstats = json_decode($results, true);
-$price = $playerstats['now_cost'];
-$playerprice = number_format($price/10, 1, '.', '');
-$teamname = $playerstats['team_name'];
-$firstname = mysql_real_escape_string($playerstats['first_name']);
-$lastname = mysql_real_escape_string($playerstats['web_name']);
-$points = $playerstats['total_points'];
-$nextmatch = $playerstats['next_fixture'];
-$position = $playerstats['type_name'];
-$selectedbynumber = $playerstats['selected_by'];
-$selectedby = number_format($selectedbynumber, 1, '.', '');
-$dreamteam = $playerstats['in_dreamteam'];
-$playerid = $playerstats['id'];
-$weekpoints = $playerstats['event_total'];
-$playershirt = $playerstats['shirt_mobile_image_url'];
-$playerphoto = $playerstats['photo_mobile_url'];
+    $endhash ='/';
+    $url = 'http://fantasy.premierleague.com/web/api/elements/';
+    $urlid = $url . $id . $endhash; 
+    $results = file_get_contents($urlid); 
+    $playerstats = json_decode($results, true);
+    $price = $playerstats['now_cost'];
+    $playerprice = number_format($price/10, 1, '.', '');
+    $teamname = $playerstats['team_name'];
+    $firstname = mysql_real_escape_string($playerstats['first_name']);
+    $lastname = mysql_real_escape_string($playerstats['web_name']);
+    $points = $playerstats['total_points'];
+    $nextmatch = $playerstats['next_fixture'];
+    $position = $playerstats['type_name'];
+    $selectedbynumber = $playerstats['selected_by'];
+    $selectedby = number_format($selectedbynumber, 1, '.', '');
+    $dreamteam = $playerstats['in_dreamteam'];
+    $playerid = $playerstats['id'];
+    $weekpoints = $playerstats['event_total'];
+    $playershirt = $playerstats['shirt_mobile_image_url'];
+    $playerphoto = $playerstats['photo_mobile_url'];
 
 
 // Enter details into database
